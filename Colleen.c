d76dc10973277935f4a7d0428bd8fd9f2d5908c3
Alain DEMENET <ademenet@student.42.fr>
2016-09-19 18:51:25 +0200

    Premier quine fait

--- Colleen.c
+++ Colleen.c
@@ -1,3 +1,5 @@
 #include <stdio.h>
-char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
-int main(void){printf(s,10,34,s,34,10,10);}
+/* comment one */
+char*s="#include <stdio.h>%c/* comment one */%cchar*s=%c%s%c;%cint main(void){/* comment two */printf(s,10,10,34,s,34,10,10,10);}%cint f(void){return(0);}%c";
+int main(void){/* comment two */printf(s,10,10,34,s,34,10,10,10);}
+int f(void){return(0);}
