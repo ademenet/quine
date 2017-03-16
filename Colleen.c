9e8b0172b9029c44b12a512b6d6bfe601cc5f9a7
John Doe <ademenet@student.42.fr>
2017-03-16 15:05:48 +0100

    Add return 0

--- Colleen.c
+++ Colleen.c
@@ -3,10 +3,10 @@
 	comment one
 */
 void f(void){
-char*s="#include <stdio.h>%c/*%c%ccomment one%c*/%cvoid f(void){%cchar*s=%c%s%c;%cprintf(s,10,10,9,10,10,10,34,s,34,10,10,10,10,9,10,10,10);}%cint main(void){%c/*%c%ccomment two%c*/%cf();}%c";
+char*s="#include <stdio.h>%c/*%c%ccomment one%c*/%cvoid f(void){%cchar*s=%c%s%c;%cprintf(s,10,10,9,10,10,10,34,s,34,10,10,10,10,9,10,10,10);}%cint main(void){%c/*%c%ccomment two%c*/%cf();return 0;}%c";
 printf(s,10,10,9,10,10,10,34,s,34,10,10,10,10,9,10,10,10);}
 int main(void){
 /*
 	comment two
 */
-f();}
+f();return 0;}
