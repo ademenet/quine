8c4afbe93cca7543b37b117415454076bf3f9ea2
John Doe <ademenet@student.42.fr>
2017-03-20 13:59:31 +0100

    Add comment as required

--- Grace.c
+++ Grace.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+/*
+	Comment one
+*/
 #define OPEN FILE *fp=fopen
 #define CLOSE fclose
-#define FT(x) int main(void){OPEN(x, "w");char *s="#include <stdio.h>%c#define OPEN FILE *fp=fopen%c#define CLOSE fclose%c#define FT(x) int main(void){OPEN(x, %cw%c);char *s=%c%s%c;fprintf(fp,s,10,10,10,34,34,34,s,34,10,34,34,10);CLOSE(fp);return 0;}%cFT(%cGrace_kid.c%c)%c";fprintf(fp,s,10,10,10,34,34,34,s,34,10,34,34,10);CLOSE(fp);return 0;}
+#define FT(x) int main(void){OPEN(x, "w");char *s="#include <stdio.h>%c/*%c%cComment one%c*/%c#define OPEN FILE *fp=fopen%c#define CLOSE fclose%c#define FT(x) int main(void){OPEN(x, %cw%c);char *s=%c%s%c;fprintf(fp,s,10,10,9,10,10,10,10,34,34,34,s,34,10,34,34,10);CLOSE(fp);return 0;}%cFT(%cGrace_kid.c%c)%c";fprintf(fp,s,10,10,9,10,10,10,10,34,34,34,s,34,10,34,34,10);CLOSE(fp);return 0;}
 FT("Grace_kid.c")
