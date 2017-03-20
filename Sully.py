13059fd9d0ccf923d4f0df7deb344be6206c142f
John Doe <ademenet@student.42.fr>
2017-03-20 15:42:40 +0100

    Sully done in python

--- Sully.py
+++ Sully.py
@@ -7,8 +7,9 @@ def ft():
 		i = i - 1
 	name = "Sully_{}.py".format(i)
 	f = open(name, "w")
-	s = "#!/usr/bin/python3{}import os{}i = {}{}def ft():{}{}global i{}{}if __file__ != {}./Sully.py{}:{}{}{}i = i - 1{}{}name = {}Sully_{}{}.py{}.format(i){}{}f = open(name, {}w{}){}{}s = {}{}{}{}{}f.write(s.format(chr(10),chr(10),i,chr(10),chr(10),chr(9),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(123),chr(125),chr(34),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(9),chr(34),chr(123),chr(125),chr(123),chr(125),chr(34),chr(10),chr(10))){}{}if i > 0:{}{}{}os.system({}chmod 777 Sully_{}{}.py; ./Sully_{}{}.py{}.format(i, i)){}ft(){}"
-	f.write(s.format(chr(10),chr(10),i,chr(10),chr(10),chr(9),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(123),chr(125),chr(34),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(9),chr(34),chr(123),chr(125),chr(123),chr(125),chr(34),chr(10),chr(10)))
+	s = "#!/usr/bin/python3{}import os{}i = {}{}def ft():{}{}global i{}{}if __file__ != {}./Sully.py{}:{}{}{}i = i - 1{}{}name = {}Sully_{}{}.py{}.format(i){}{}f = open(name, {}w{}){}{}s = {}{}{}{}{}f.write(s.format(chr(10),chr(10),i,chr(10),chr(10),chr(9),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(123),chr(125),chr(34),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(9),chr(34),chr(123),chr(125),chr(123),chr(125),chr(34),chr(10),chr(10))){}{}f.close(){}{}if i > 0:{}{}{}os.system({}chmod 777 Sully_{}{}.py; ./Sully_{}{}.py{}.format(i, i)){}ft(){}"
+	f.write(s.format(chr(10),chr(10),i,chr(10),chr(10),chr(9),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(123),chr(125),chr(34),chr(10),chr(9),chr(34),chr(34),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(10),chr(9),chr(9),chr(34),chr(123),chr(125),chr(123),chr(125),chr(34),chr(10),chr(10)))
+	f.close()
 	if i > 0:
 		os.system("chmod 777 Sully_{}.py; ./Sully_{}.py".format(i, i))
 ft()
