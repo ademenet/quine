ea0c2916b357b187cfeb41eba1f8f6460136dfdc
John Doe <ademenet@student.42.fr>
2017-03-20 13:45:09 +0100

    Update Colleen in python and delete README file

--- Colleen.py
+++ Colleen.py
@@ -1,11 +1,10 @@
-# TODO remplacer les triples " par des triples chr(34)
 #!/usr/bin/python3
 """
 	Comment one
 """
 def f():
-	s = "#!/usr/bin/python{}{}{}{}Comment one{}{}{}def f():{}{}s = {}{}{}{}{}print(s.format(chr(10),'\"\"\"',chr(10),chr(9),chr(10),'\"\"\"',chr(10),chr(10),chr(9),'\"',s,'\"',chr(10),chr(9),chr(10),chr(10),chr(10),chr(9),'\"\"\"',chr(10),chr(9),chr(9),chr(10),chr(9),'\"\"\"',chr(10),chr(9),chr(10),chr(10),chr(10))){}{}def main():{}{}{}{}{}{}Comment two{}{}{}{}{}f(){}{}main(){}"
-	print(s.format(chr(10),'"""',chr(10),chr(9),chr(10),'"""',chr(10),chr(10),chr(9),'"',s,'"',chr(10),chr(9),chr(10),chr(10),chr(10),chr(9),'"""',chr(10),chr(9),chr(9),chr(10),chr(9),'"""',chr(10),chr(9),chr(10),chr(10),chr(10)))
+	s = "#!/usr/bin/python3{}{}{}{}{}{}Comment one{}{}{}{}{}def f():{}{}s = {}{}{}{}{}print(s.format(chr(10),chr(34),chr(34),chr(34),chr(10),chr(9),chr(10),chr(34),chr(34),chr(34),chr(10),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(10),chr(10),chr(9),chr(34),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(34),chr(34),chr(10),chr(9),chr(10),chr(10))){}{}def main():{}{}{}{}{}{}{}{}Comment two{}{}{}{}{}{}{}f(){}{}main()"
+	print(s.format(chr(10),chr(34),chr(34),chr(34),chr(10),chr(9),chr(10),chr(34),chr(34),chr(34),chr(10),chr(10),chr(9),chr(34),s,chr(34),chr(10),chr(9),chr(10),chr(10),chr(10),chr(9),chr(34),chr(34),chr(34),chr(10),chr(9),chr(9),chr(10),chr(9),chr(34),chr(34),chr(34),chr(10),chr(9),chr(10),chr(10)))
 
 def main():
 	"""
